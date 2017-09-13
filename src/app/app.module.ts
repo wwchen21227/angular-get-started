5ddf0cfe2c5c7a29032f5f2076d3523a658dbee1
Chen Weng Wei <wwchen21227@gmail.com>
2017-09-13 21:54:52 +0800

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from "./products/product-list.component";
+import { ConverToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConverToSpacesPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
